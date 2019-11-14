7c986bb0da6a09628b00612e56c6b70bc03f8b88
Markus Haslinger <markus.haslinger@virtrel.com>
2019-11-14 23:40:01 +0100

    Revert "Graphical User Interface"
    This reverts commit 1438d3f031575c2b98213e0bb819cee7da49a014.

--- view.js
+++
@@ -1,8 +0,0 @@
-let $ = require('jquery')
-
-$('#console').append("CH 1<br>")
-$('#console').append("SOURCE:VOLT:MAXI 35<br>")
-$('#console').append("SOURCE:CURR:MAXI 45<br>")
-$('#console').append("SOURCE:CURR 45<br>")
-$('#console').append("24.8 VOLT --> ABSCHALTUNG NACH 2 SEK<br>")
-$('#console').append("SOURCE:VOLT 24.8<br>")
